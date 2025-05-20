7a19382ba42367fe9355ec69ebb6e992155ded8f
YaeMonilc <monilc@outlook.com>
2025-05-20 21:47:31 +0800

    移除不需要的内容

--- app/src/main/java/ltd/aliothstar/blackshoresbox/ui/activity/MainActivity.kt
+++ app/src/main/java/ltd/aliothstar/blackshoresbox/ui/activity/MainActivity.kt
@@ -5,16 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
-import jakarta.inject.Inject
-import ltd.aliothstar.blackshoresbox.network.KuroApi
-import ltd.aliothstar.blackshoresbox.network.KuroApiInterface
 import ltd.aliothstar.blackshoresbox.ui.theme.BlackShoresBoxTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    @KuroApi
-    lateinit var kuroApi: KuroApiInterface
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
