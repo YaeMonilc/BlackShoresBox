96064c35f9e3119eb3f8b5a2ae8c4b3e53764cd6
YaeMonilc <monilc@outlook.com>
2025-05-20 22:40:07 +0800

    增加资源

--- app/src/main/java/ltd/aliothstar/blackshoresbox/util/GameAssetsUtils.kt
+++ app/src/main/java/ltd/aliothstar/blackshoresbox/util/GameAssetsUtils.kt
@@ -9,7 +9,15 @@ import androidx.compose.ui.platform.LocalContext
 enum class GameResource(
     val path: String
 ) {
-
+    IMG_UI_ITEM_BG("game/resource/img_ui_item_bg.png"),
+    IMG_UI_ITEM_QUALITY_BLUE("game/resource/img_ui_item_quality_blue.png"),
+    IMG_UI_ITEM_QUALITY_COLOR("game/resource/img_ui_item_quality_color.png"),
+    IMG_UI_ITEM_QUALITY_GOLD("game/resource/img_ui_item_quality_gold.png"),
+    IMG_UI_ITEM_QUALITY_GRAY("game/resource/img_ui_item_quality_gray.png"),
+    IMG_UI_ITEM_QUALITY_GREEN("game/resource/img_ui_item_quality_green.png"),
+    IMG_UI_ITEM_QUALITY_PURPLE("game/resource/img_ui_item_quality_purple.png"),
+    IMG_UI_ITEM_QUALITY_RED("game/resource/img_ui_item_quality_red.png")
+    ;
 }
 
 @Composable
