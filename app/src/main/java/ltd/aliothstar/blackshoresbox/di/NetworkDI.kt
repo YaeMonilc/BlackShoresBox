20e5657867de1d9ecea2639fe1116cd24e412533
YaeMonilc <monilc@outlook.com>
2025-05-21 14:13:46 +0800

    全局拦截Kuro Token过期事件，并设置状态

--- app/src/main/java/ltd/aliothstar/blackshoresbox/di/NetworkDI.kt
+++ app/src/main/java/ltd/aliothstar/blackshoresbox/di/NetworkDI.kt
@@ -2,18 +2,21 @@ package ltd.aliothstar.blackshoresbox.di
 
 import android.content.Context
 import android.os.Build
+import androidx.room.util.copy
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import ltd.aliothstar.blackshoresbox.network.KURO_API_BASE_URL
 import ltd.aliothstar.blackshoresbox.network.KuroApi
 import ltd.aliothstar.blackshoresbox.network.KuroApiInterface
 import ltd.aliothstar.blackshoresbox.network.KuroBaseResponse
+import ltd.aliothstar.blackshoresbox.repository.UserAuthStateRepository
 import ltd.aliothstar.blackshoresbox.util.nonStandardKuroResponseProcess
 import ltd.aliothstar.blackshoresbox.util.randomText
 import okhttp3.MediaType.Companion.toMediaType
@@ -32,7 +35,8 @@ class NetworkDI {
     fun provideKuroApi(
         @ApplicationContext
         context: Context,
-        json: Json
+        json: Json,
+        userAuthStateRepository: UserAuthStateRepository
     ): KuroApiInterface =
         Retrofit.Builder().apply {
             baseUrl(KURO_API_BASE_URL)
@@ -55,6 +59,16 @@ class NetworkDI {
                             body.string().nonStandardKuroResponseProcess().let { string ->
                                 json.decodeFromString<KuroBaseResponse<JsonElement>>(string).let { response ->
                                     if (response.code != KuroBaseResponse.Code.SUCCESS) {
+                                        if (response.code == KuroBaseResponse.Code.TOKEN_EXPIRED) {
+                                            runBlocking {
+                                                userAuthStateRepository.setAuthState {
+                                                    token = ""
+                                                    userId = 0
+                                                    isLoggedIn = false
+                                                }
+                                            }
+                                        }
+
                                         KuroBaseResponse.ErrorResponse.fromCode(response.code)?.let { exception ->
                                             throw exception.apply {
                                                 this.body = string
