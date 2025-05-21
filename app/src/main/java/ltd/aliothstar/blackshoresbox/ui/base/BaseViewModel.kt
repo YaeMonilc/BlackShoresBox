2e0ab0f2dd617e539aef30306b99f553f7d871c1
YaeMonilc <monilc@outlook.com>
2025-05-21 20:31:14 +0800

    修复问题

--- app/src/main/java/ltd/aliothstar/blackshoresbox/ui/base/BaseViewModel.kt
+++ app/src/main/java/ltd/aliothstar/blackshoresbox/ui/base/BaseViewModel.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.launch
 
 open class State
@@ -18,14 +19,6 @@ open class Effect
 abstract class BaseViewModel<S: State, I: Intent, E: Effect> : ViewModel() {
     abstract fun initializeState(): S
 
-    init {
-        viewModelScope.launch {
-            intent.collect {
-                handleIntent(it)
-            }
-        }
-    }
-
     private val _state = mutableStateOf(initializeState())
     val state: androidx.compose.runtime.State<S> = _state
 
@@ -33,15 +26,22 @@ abstract class BaseViewModel<S: State, I: Intent, E: Effect> : ViewModel() {
         _state.value = state.value.intent()
     }
 
-    private val _intent by lazy { MutableSharedFlow<I>() }
-    private val intent: SharedFlow<I> by lazy { _intent }
+    private val _intent by lazy { Channel<I>() }
 
     abstract fun handleIntent(intent: I)
 
-    fun emitIntent(intent: () -> I) = _intent.tryEmit(intent())
+    fun emitIntent(intent: () -> I) = _intent.trySend(intent())
 
-    private val _effect by lazy { Channel<E>() }
-    val effect: SendChannel<E> by lazy { _effect }
+    private val _effect by lazy { MutableSharedFlow<E>() }
+    val effect: SharedFlow<E> by lazy { _effect }
 
-    protected fun emitEffect(effect: () -> E) = _effect.trySend(effect())
+    protected fun emitEffect(effect: () -> E) = _effect.tryEmit(effect())
+
+    init {
+        viewModelScope.launch {
+            _intent.consumeAsFlow().collect {
+                handleIntent(it)
+            }
+        }
+    }
 }
