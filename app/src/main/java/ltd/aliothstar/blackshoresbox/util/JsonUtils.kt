c7b7ee1187a6e6057f5dcad9f10366ba66826b20
YaeMonilc <monilc@outlook.com>
2025-05-21 20:38:48 +0800

    修复部分响应并无data字段所造成的异常

--- app/src/main/java/ltd/aliothstar/blackshoresbox/util/JsonUtils.kt
+++ app/src/main/java/ltd/aliothstar/blackshoresbox/util/JsonUtils.kt
@@ -4,12 +4,16 @@ import org.json.JSONObject
 
 fun String.nonStandardKuroResponseProcess(): String =
     JSONObject(this).apply {
-        val element = get("data")
+        try {
+            val element = get("data")
 
-        if (element is String) {
-            if (element.startsWith("{") && element.endsWith("}")) {
-                remove("data")
-                put("data", JSONObject(element))
+            if (element is String) {
+                if (element.startsWith("{") && element.endsWith("}")) {
+                    remove("data")
+                    put("data", JSONObject(element))
+                }
             }
+        } catch (_: Exception) {
+
         }
     }.toString()
