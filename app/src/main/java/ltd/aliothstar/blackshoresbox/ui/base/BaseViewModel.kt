a26b5c876815c7442c7e4ac2132e4de490d8fdc8
YaeMonilc <monilc@outlook.com>
2025-05-21 21:31:42 +0800

    移除未使用的引用

--- app/src/main/java/ltd/aliothstar/blackshoresbox/ui/base/BaseViewModel.kt
+++ app/src/main/java/ltd/aliothstar/blackshoresbox/ui/base/BaseViewModel.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.consumeAsFlow
